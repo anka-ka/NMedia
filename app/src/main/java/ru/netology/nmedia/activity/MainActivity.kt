05a9a7e3a5df71eed16ad432abdbec35b8881994
kaz <kazan19066@gmail.com>
2024-04-08 17:31:38 +0400

    added OnShareListener

--- app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import androidx.activity.viewModels
+import ru.netology.nmedia.adapter.PostAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 
@@ -16,49 +16,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
+        val adapter = PostAdapter({
+            viewModel.likeById(it.id)
+        },{
+            viewModel.shareById(it.id)
+        })
+        binding.list?.adapter = adapter
         viewModel.data.observe(this) { posts ->
-            posts.map { post ->
-                binding.container?.removeAllViews()
-                CardPostBinding.inflate(layoutInflater, binding.container, false).apply {
-                    author.text = post.author
-                    published.text = post.published
-                    content.text = post.content
-                    NumberOfLikes.setImageResource(
-                        if (post.likedByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24
-                )
-                    NumberOfLikes.setOnClickListener{
-                        viewModel.likeById(post.id)
-                    }
-                }
-            }
+            adapter.submitList(posts)
         }
     }
 }
-//            with(binding) {
-//                author.text = post.author
-//                published.text = post.published
-//                content.text = post.content
-//                likes.text = viewModel.cutLongNumbers(post.likes)
-//                shares.text = viewModel.cutLongNumbers(post.shares)
-//                NumberOfLikes.setImageResource(
-//                    if (post.likedByMe) R.drawable.baseline_favorite_24 else R.drawable.baseline_favorite_border_24
-//                )
-//                binding.NumberOfLikes.setOnClickListener {
-//                    if (post.likedByMe) {
-//                        post.likes--
-//                    } else {
-//                        post.likes++
-//                    }
-//                    viewModel.like()
-//                    likes.text = viewModel.cutLongNumbers(post.likes)
-//                }
-//
-//                binding.NumberOfShares.setOnClickListener {
-//                    post.shares++
-//                    viewModel.share()
-//                    shares.text = viewModel.cutLongNumbers(post.shares)
-//                }
-//            }
-//        }
-//    }
-//}
--- app/src/main/java/ru/netology/nmedia/viewmodel/ViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/ViewModel.kt
@@ -12,15 +12,5 @@ class PostViewModel : ViewModel() {
 
     val data = repository.getAll()
     fun likeById(id: Long) = repository.likeById(id)
-   // fun share() = repository.share()
-
-    fun cutLongNumbers(number: Int): String {
-        val formattedNumber = when {
-            number < 1000 -> number.toString()
-            number < 10_000 -> "${number / 1000}.${(number % 1000) / 100}K"
-            number in 10_000..1_000_000 -> (number / 1000).toString() + "K"
-            else -> "${number / 1_000_000}.${number % 1_000_000 / 100_000}M"
-        }
-        return formattedNumber
-    }
+    fun shareById(id: Long) = repository.shareById(id)
 }
